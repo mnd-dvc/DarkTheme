ecf356277220eae2c09a219e52ac5fa0203afa44
Mehmet Nur Devecioğlu <mnd.dvc@gmail.com>
2023-09-26 17:42:32 +0300

    Gulp.js Kullanımı

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useThemeSwitcher } from "react-css-theme-switcher";
+import "./App.css";
+import SwitchButton from "./components/SwitchButton";
 
 function App() {
+  const { currentTheme } = useThemeSwitcher();
+  console.log(currentTheme);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="myButton">
+      <h1 style={{ marginLeft: "-40%" }}>
+        The current theme is {currentTheme}
+      </h1>
+      <SwitchButton />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { ThemeSwitcherProvider } from "react-css-theme-switcher";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const themes = {
+  dark: `${process.env.PUBLIC_URL}/dark-theme.css`,
+  light: `${process.env.PUBLIC_URL}/light-theme.css`,
+};
+
+if (!localStorage.getItem("theme")) {
+  localStorage.setItem("theme", "dark");
+}
+
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <ThemeSwitcherProvider
+      themeMap={themes}
+      defaultTheme={localStorage.getItem("theme")}
+      insertionPoint={document.getElementById("inject-styles-here")}
+    >
+      <App />
+    </ThemeSwitcherProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
